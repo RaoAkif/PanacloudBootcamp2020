77edc568c0a3f530dd4f96df1be6b55e39543737
RaoAkif <61361037+RaoAkif@users.noreply.github.com>
2020-12-31 01:59:10 +0500

    011_TaskboxReactComponentTypeScriptStorybookBit

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    '../src/components/**/*.stories.js'
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "./design-addon/register.js"
+  ]
+}
